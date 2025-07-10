dad373c2328337ce019ba7695817212e03154584
h3ithor <heitorfelipe37@gmail.com>
2025-07-10 16:44:35 -0300

    feat: add fields from raw data

--- src/services/24-1_CC.ts
+++ src/services/24-1_CC.ts
@@ -2,7 +2,8 @@ import puppeteer from "puppeteer";
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    // headless: false,
+    // devtools: true,
   });
   const page = await browser.newPage();
 
@@ -25,14 +26,57 @@ import puppeteer from "puppeteer";
   await page.waitForSelector(link);
   await page.click(link);
 
-  const data = [];
   const elementsArray = await page.evaluate(() => {
     const ol = document.querySelector("body > div > ol");
 
     const listItems = Array.from(ol.querySelectorAll("li"));
-    console.log(listItems);
+    console.log(listItems.map((el) => el.innerHTML));
     return listItems.map((el) => el.textContent);
   });
+
+  const HTMLelementsArray = await page.evaluate(() => {
+    const ol = document.querySelector("body > div > ol");
+
+    const listItems = Array.from(ol.querySelectorAll("li"));
+    return listItems.map((el) => el.innerHTML);
+  });
+  // just to take as reference
+  //   export interface ScrapedData {
+  //   raw: string;
+  //   title: string;
+  //   tg?: string;
+  //   Initial_proposal: string;
+  //   Author: string;
+  //   Course: string;
+  //   Advisor: string;
+  //   Co_Advisor?: string;
+  //   Possible_appraiser?: string;
+  //   Proposal_abstract: string;
+  //   date: string;
+  //   evaluation_panel: string;
+  //   local?: string;
+  //   key_words?: string;
+  // }
+  const raw = elementsArray;
+  const HTMLraw = HTMLelementsArray;
+  const title = raw.map((el) =>
+    el.slice(el.indexOf("Título:") + 7, el.indexOf("\n      TG: ")).trim()
+  );
+  const tg = HTMLraw.map((el) => {
+    const hrefStart = el.indexOf('href="');
+    const hrefEnd = el.indexOf('"', hrefStart + 6);
+
+    if (hrefStart !== -1 && hrefEnd !== -1) {
+      const relativePath = el.slice(hrefStart + 6, hrefEnd);
+      return `https://www.cin.ufpe.br/~tg/2024-1/${relativePath}`;
+    }
+
+    return null;
+  });
+
+  // console.log(elementsArray);
+  console.log(title);
+  console.log(tg);
 
   await browser.close();
 })();
