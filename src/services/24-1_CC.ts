cd81191664b0aeaaf69bd21aeb3fc7c042da336b
h3ithor <heitorfelipe37@gmail.com>
2025-07-14 17:34:58 -0300

    feat: export 24-1 data to bd

--- src/services/24-1_CC.ts
+++ src/services/24-1_CC.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import { ScrapedData } from "src/types/scraping.types";
+import { DataRepository } from "../repositories/scrapingRepository";
 
 (async () => {
   const browser = await puppeteer.launch({});
@@ -20,7 +21,7 @@ import { ScrapedData } from "src/types/scraping.types";
   });
 
   const link =
-    "body > div > div:nth-child(12) > table > tbody > tr:nth-child(1) > td:nth-child(1) > h3 > u > span > a";
+    "body > div > div:nth-child(12) > table > tbody > tr:nth-child(1) > td:nth-child(1) > h3 > u > span > a"; // seletor css do ano
   await page.waitForSelector(link);
   await page.click(link);
 
@@ -52,12 +53,13 @@ import { ScrapedData } from "src/types/scraping.types";
           .replaceAll("<br>", "")
           .split("</b>")
           .map((v1) => {
-            if (v1.trim().startsWith("<a"))
-              return `https://cin.ufpe.br/~tg/2024-1/${(
-                v1.match(/href="([^"]*)"/)?.[1] || ""
-              )
+            if (v1.trim().startsWith("<a")) {
+              const re = /href="([^"]*)"/;
+              const match = re.exec(v1);
+              return `https://cin.ufpe.br/~tg/2024-1/${(match?.[1] || "")
                 .replaceAll('"', "")
                 .replace("href=", "")}`;
+            }
             return v1.trim();
           })
       )
@@ -76,35 +78,60 @@ import { ScrapedData } from "src/types/scraping.types";
   const banca = newRaw2.map((v) => v[10][1]);
   const semestre = "2024-1";
 
-  const dia = apresentacao.map((v) =>
-    v.slice(v.indexOf("dia: ") + 5, v.indexOf(","))
-  );
-
-  const hora = apresentacao.map((v) => {
-    const match = v.match(/hora: ([^,]+)/);
-    return match ? match[1].trim() : "";
+  const dia = apresentacao.map((v) => {
+    v = v.slice(v.indexOf("dia: ") + 5, v.indexOf(","));
+    if (v.trim() === "DD/MM/AAAA") {
+      return null;
+    }
+    return v;
   });
 
-  const local = apresentacao.map((v) => v.slice(v.indexOf("local: ") + 7));
+  const hora = apresentacao.map((v) => {
+    const re = /hora: ([^,]+)/;
+    const match = re.exec(v);
+    const newMatch = match[1].trim();
+    if (newMatch.trim() === "XXhYY") {
+      return null;
+    }
+    return newMatch || null;
+  });
+
+  const local = apresentacao.map((v) => {
+    v = v.slice(v.indexOf("local: ") + 7);
+    if (v.trim() === "LLLL") {
+      return null;
+    }
+    return v;
+  });
 
   const scrapedDataArray: ScrapedData[] = elementsArray.map(
     (rawText, index) => ({
       title: titulo[index],
       tg: tg[index],
-      Initial_proposal: propostaInicial[index],
-      Author: autor[index],
-      Course: curso[index],
-      Advisor: orientador[index],
-      Co_Advisor: coorientador[index],
-      Possible_appraiser: possiveisAvaliadores[index],
-      Proposal_abstract: resumoDaProposta[index],
+      initial_proposal: propostaInicial[index],
+      author: autor[index],
+      course: curso[index],
+      advisor: orientador[index],
+      co_Advisor: coorientador[index],
+      possible_appraiser: possiveisAvaliadores[index],
+      proposal_abstract: resumoDaProposta[index],
       evaluation_panel: banca[index],
       semester: semestre,
       day: dia[index],
       hour: hora[index],
       local: local[index],
+      year: "24-1",
     })
   );
+
+  try {
+    const data = new DataRepository();
+    await data.saveMany(scrapedDataArray);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("finished");
+  }
 
   await browser.close();
 })();
