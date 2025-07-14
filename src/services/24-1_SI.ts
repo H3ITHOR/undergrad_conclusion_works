c5f91d6e7146beeb68b9d292ba8f98a01e20cf66
h3ithor <heitorfelipe37@gmail.com>
2025-07-14 19:02:52 -0300

    feat: split SI 24-1 tgs into an array

---
+++ src/services/24-1_SI.ts
@@ -0,0 +1,183 @@
+import puppeteer from "puppeteer";
+import { ScrapedData } from "src/types/scraping.types";
+import { DataRepository } from "../repositories/scrapingRepository";
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://cin.ufpe.br/~tg/");
+
+  const resolution = await page.evaluate(() => {
+    return {
+      width: window.screen.width,
+      height: window.screen.height,
+    };
+  });
+
+  await page.setViewport({
+    width: resolution.width,
+    height: resolution.height,
+  });
+
+  const link =
+    "body > div > div:nth-child(12) > table > tbody > tr:nth-child(1) > td:nth-child(1) > h3 > u > span > a"; // seletor css do ano
+  await page.waitForSelector(link);
+  await page.click(link);
+
+  const bigString = await page.evaluate(() => {
+    const body = document.querySelector("body");
+
+    const body2 = document.body.innerText;
+    const SI = body2.slice(body2.indexOf("rtbvvpt") + 15).split("\n");
+    const raws: string[] = [];
+
+    for (let i = 0; i < SI.length; i++) {
+      if (SI[i].startsWith("Título:")) {
+        let raw = [SI[i]];
+        i++;
+
+        while (i < SI.length && !SI[i].startsWith("Banca:")) {
+          raw.push(SI[i]);
+          i++;
+        }
+
+        if (i < SI.length && SI[i].startsWith("Banca:")) {
+          raw.push(SI[i]);
+        }
+
+        raws.push(raw.join("\n"));
+      }
+    }
+    return raws;
+    // const allElements = Array.from(body.querySelectorAll("*"));
+
+    // const siIndex = allElements.findIndex((el) =>
+    //   el.textContent.includes("Sistemas de Informação")
+    // );
+
+    // const elementsAfterSI = allElements.slice(siIndex + 1);
+
+    // // return elementsAfterSI.map((el) => el.textContent || "").join("\n");
+    // return allElements;
+  });
+
+  const elementsArray = await page.evaluate(() => {
+    const ol = document.querySelector("body");
+
+    const SIPage = ol.querySelectorAll("b");
+
+    const listItems = Array.from(ol.querySelectorAll("b"));
+    const SIlistItems = listItems.slice(
+      listItems.findIndex((el) => el.innerText === "Sistemas de Informação") + 1
+    );
+
+    // const raw = SIlistItems.map(v => )
+    console.log(SIlistItems.map((el) => el.innerText));
+    return SIlistItems.map((el) => el.textContent.replace(/\n/g, " ").trim());
+  });
+
+  const HTMLelementsArray = await page.evaluate(() => {
+    const ol = document.querySelector("body > b");
+
+    const listItems = Array.from(ol.querySelectorAll("li"));
+    return listItems.map((el) => el.innerHTML);
+  });
+
+  const newRaw2 = HTMLelementsArray.map((v) =>
+    v
+      .replace(/Resumo da Proposta:<\/b>\n/g, "Resumo da Proposta:</b>")
+      .replace(/\n/g, " ")
+      .trim()
+      .split("<br><b>")
+      .map((v1) =>
+        v1
+          .trim()
+          .replaceAll("<b>", "")
+          .replaceAll("<br>", "")
+          .split("</b>")
+          .map((v1) => {
+            if (v1.trim().startsWith("<a")) {
+              const re = /href="([^"]*)"/;
+              const match = re.exec(v1);
+              return `https://cin.ufpe.br/~tg/2024-1/${(match?.[1] || "")
+                .replaceAll('"', "")
+                .replace("href=", "")}`;
+            }
+            return v1.trim();
+          })
+      )
+  );
+
+  const titulo = newRaw2.map((v) => v[0][1]);
+  const tg = newRaw2.map((v) => v[1][1]);
+  const propostaInicial = newRaw2.map((v) => v[2][1]);
+  const autor = newRaw2.map((v) => v[3][1]);
+  const curso = newRaw2.map((v) => v[4][1]);
+  const orientador = newRaw2.map((v) => v[5][1]);
+  const coorientador = newRaw2.map((v) => v[6][1]);
+  const possiveisAvaliadores = newRaw2.map((v) => v[7][1]);
+  const resumoDaProposta = newRaw2.map((v) => v[8][1]);
+  const apresentacao = newRaw2.map((v) => v[9][1]);
+  const banca = newRaw2.map((v) => v[10][1]);
+  const semestre = "2024-1";
+
+  const dia = apresentacao.map((v) => {
+    v = v.slice(v.indexOf("dia: ") + 5, v.indexOf(","));
+    if (v.trim() === "DD/MM/AAAA") {
+      return null;
+    }
+    return v;
+  });
+
+  const hora = apresentacao.map((v) => {
+    const re = /hora: ([^,]+)/;
+    const match = re.exec(v);
+    const newMatch = match[1].trim();
+    if (newMatch.trim() === "XXhYY") {
+      return null;
+    }
+    return newMatch || null;
+  });
+
+  const local = apresentacao.map((v) => {
+    v = v.slice(v.indexOf("local: ") + 7);
+    if (v.trim() === "LLLL") {
+      return null;
+    }
+    return v;
+  });
+
+  const scrapedDataArray: ScrapedData[] = elementsArray.map(
+    (rawText, index) => ({
+      title: titulo[index],
+      tg: tg[index],
+      initial_proposal: propostaInicial[index],
+      author: autor[index],
+      course: curso[index],
+      advisor: orientador[index],
+      co_Advisor: coorientador[index],
+      possible_appraiser: possiveisAvaliadores[index],
+      proposal_abstract: resumoDaProposta[index],
+      evaluation_panel: banca[index],
+      semester: semestre,
+      day: dia[index],
+      hour: hora[index],
+      local: local[index],
+      year: "24-1",
+    })
+  );
+
+  try {
+    const data = new DataRepository();
+    await data.saveMany(scrapedDataArray);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("finished");
+  }
+
+  await browser.close();
+})();
